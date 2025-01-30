7c96dd2e3293e2ae020a91ad4aea804afea45c8b
tifufu <dematasjoshua@hotmail.com>
2025-01-30 11:21:19 +0100

    feat: cache svg in file

--- activity/client.go
+++ activity/client.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"sync"
+	"sync/atomic"
 	"text/template"
 
 	"github.com/charmbracelet/log"
@@ -27,14 +29,21 @@ type Client struct {
 	logger *log.Logger
 
 	currentActivity Activity
-	dirty           bool
+	dirty           atomic.Bool
 	mu              sync.RWMutex
+	fileMu          sync.Mutex
 }
 
 func NewClient(logger *log.Logger, apiKey string) *Client {
 	assert.AssertNotNil(logger)
 	assert.AssertNotEmpty(apiKey)
-	return &Client{
+
+	err := os.MkdirAll("data", 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &Client{
 		logger: logger,
 		apiKey: apiKey,
 		currentActivity: Activity{
@@ -44,8 +53,10 @@ func NewClient(logger *log.Logger, apiKey string) *Client {
 			Url:          "https://www.joshuadematas.me/",
 			ThumbnailUrl: "https://i.pinimg.com/736x/71/eb/50/71eb502aea2fc4e816b67a5bbd114d27.jpg",
 		},
-		mu: sync.RWMutex{},
 	}
+
+	client.dirty.Store(true)
+	return client
 }
 
 func (c *Client) SetYoutubeActivity(ctx context.Context, videoId string) error {
@@ -81,16 +92,39 @@ func (c *Client) setActivity(a Activity) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.currentActivity = a
-	c.dirty = true
+	c.dirty.Store(true)
 }
 
 func (c *Client) StreamSVG(ctx context.Context, out io.Writer) error {
+	if !c.dirty.Load() {
+		file, err := os.Open("data/activity.svg")
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(out, file)
+		return err
+	}
+
+	c.logger.Info("activity dirty, re-building SVG")
+
+	c.fileMu.Lock()
+	defer c.fileMu.Lock()
+
+	file, err := os.OpenFile("data/activity.svg", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	out = io.MultiWriter(file, out)
+
 	templates, err := template.ParseFiles("./activity/templates/.template.svg")
 	if err != nil {
 		return err
 	}
 
 	activity := c.Activity()
+	c.logger.Debug("downloading activity thumb", "id", activity.Id, "url", activity.ThumbnailUrl)
 	base64Image, err := downloadBase64Image(ctx, activity.ThumbnailUrl)
 	if err != nil {
 		return fmt.Errorf("image download: %s", err)
@@ -106,7 +140,13 @@ func (c *Client) StreamSVG(ctx context.Context, out io.Writer) error {
 		ExternalLink: activity.Url,
 	}
 
-	return templates.ExecuteTemplate(out, ".template.svg", input)
+	err = templates.ExecuteTemplate(out, ".template.svg", input)
+	if err != nil {
+		return err
+	}
+
+	c.dirty.Store(false)
+	return nil
 }
 
 func downloadBase64Image(ctx context.Context, url string) (string, error) {
