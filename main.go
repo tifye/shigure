1239af8cf81cefc49588678448804d2f0d123ce2
tifufu <dematasjoshua@hotmail.com>
2025-02-02 20:43:26 +0100

    feat: read port from env

--- main.go
+++ main.go
@@ -41,7 +41,10 @@ func main() {
 }
 
 func run(ctx context.Context, logger *log.Logger, config *viper.Viper) error {
-	ln, err := net.Listen("tcp", "0.0.0.0:6565")
+	config.SetDefault("PORT", 8080)
+	port := config.GetInt("PORT")
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return fmt.Errorf("net listen: %s", err)
 	}
