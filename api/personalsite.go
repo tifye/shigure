4849015a9862bb7d0db987d36f17e54a3f9244e5
tifufu <dematasjoshua@hotmail.com>
2025-06-22 23:54:27 +0200

    feat: notify discord on user room join

--- api/personalsite.go
+++ api/personalsite.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/charmbracelet/log"
@@ -23,7 +27,13 @@ type PositionData struct {
 }
 
 func handlePersonalSiteRoom(logger *log.Logger, room *personalsite.RoomHub) echo.HandlerFunc {
+	type request struct {
+		IsMe bool `query:"isme"`
+	}
 	return func(c echo.Context) error {
+		var req request
+		_ = c.Bind(&req)
+
 		conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 		if err != nil {
 			logger.Error(err)
@@ -40,7 +50,13 @@ func handlePersonalSiteRoom(logger *log.Logger, room *personalsite.RoomHub) echo
 		room.Register(user)
 		defer room.Unregister(user)
 
-		logger.Info("client connected")
+		logger.Info("Client connected", "isme", req.IsMe)
+
+		if !req.IsMe {
+			if err = notifyDiscord(c.Request().Context()); err != nil {
+				logger.Error("Failed to send Discord notification", "err", err, "isme", req.IsMe)
+			}
+		}
 
 		go func() {
 			for msg := range user.WriteChan {
@@ -63,4 +79,30 @@ func handlePersonalSiteRoom(logger *log.Logger, room *personalsite.RoomHub) echo
 
 		return c.NoContent(http.StatusOK)
 	}
+}
+
+const discordWebhookURL = "https://discord.com/api/webhooks/1386429934603468901/q9lyRkp0812_HbHrG1ZjRNIghZILETgxVtP5yEvUEv7CugBgTLafsrkNqANP3rxSZDf9"
+
+type webhookBody struct {
+	Content string `json:"content"`
+}
+
+func notifyDiscord(ctx context.Context) error {
+	body := webhookBody{Content: "Someone joined the room https://www.joshuadematas.me"}
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("marhsall body: %s", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", discordWebhookURL, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("webhook res: %s", err)
+	}
+	res.Body.Close()
+	return nil
 }
