809d53d1f357267a18850da417bda5c863547eeb
tifufu <dematasjoshua@hotmail.com>
2025-07-05 23:25:47 +0200

    "."

--- activity/vscode.go
+++ activity/vscode.go
@@ -64,7 +64,9 @@ func NewVSCodeActivityClient(logger *log.Logger) *VSCodeActivityClient {
 func (c *VSCodeActivityClient) SetActivity(a VSCodeActivity) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	c.logger.Debug(a.Filename)
 	a.Filename = filepath.Base(a.Filename)
+	c.logger.Debug(a.Filename)
 	c.activity = a
 	c.lastUpdate = time.Now()
 }
