5e07c84a14a1597d20f7b80ad5abbee098fef3f8
tifufu <dematasjoshua@hotmail.com>
2025-07-24 22:48:52 +0200

    refactor: moved cleanupfuncs def to main file Done purely to build easier in Cloud Build because of how it targets ./main to build

--- main.go
+++ main.go
@@ -132,3 +132,19 @@ func initDependencies(logger *log.Logger, config *viper.Viper) (deps *api.Server
 		WSMux:                mux,
 	}, cfs, nil
 }
+
+type CleanupFuncs []func() error
+
+func (cf *CleanupFuncs) Defer(f func() error) {
+	*cf = append(*cf, f)
+}
+
+func (cf *CleanupFuncs) Cleanup() error {
+	errs := make([]error, 0)
+	for i := len(*cf) - 1; i >= 0; i-- {
+		if ferr := (*cf)[i](); ferr != nil {
+			errs = append(errs, ferr)
+		}
+	}
+	return errors.Join(errs...)
+}
