fa3ddc62233ff7d4277476859b96f91c6e36aa23
tifufu <dematasjoshua@hotmail.com>
2025-08-13 21:40:40 +0200

    fix: seed flags are now used if set

--- muxsimulator/main.go
+++ muxsimulator/main.go
@@ -16,6 +16,25 @@ func main() {
 	flag.Uint64Var(&seed2, "seed2", 0, "Second seed value")
 	flag.Parse()
 
-	sim := NewSimulator(rand.Uint64(), rand.Uint64(), os.Stdout)
+	wasSeed1Set := false
+	wasSeed2Set := false
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "seed1" {
+			wasSeed1Set = true
+		}
+		if f.Name == "seed2" {
+			wasSeed2Set = true
+		}
+	})
+
+	if !wasSeed1Set {
+		seed1 = rand.Uint64()
+	}
+	if !wasSeed2Set {
+		seed2 = rand.Uint64()
+	}
+
+	sim := NewSimulator(seed1, seed2, os.Stdout)
 	sim.Run()
 }
